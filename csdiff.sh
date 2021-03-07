9381146e31740e991ed1174f41ea90dcd09b8e13
Jônatas Clementino <jonatas.clementino@luizalabs.com>
2021-03-07 15:24:45 -0300

    csdiff: change new line marker, use sed instead of python script and use sed command to fix conflict output

--- csdiff.sh
+++ csdiff.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sed 's/{/\n$${\n$$/g' $1 | sed 's/}/\n$$}\n$$/g' > "temp_$1"
-sed 's/{/\n$${\n$$/g' $2 | sed 's/}/\n$$}\n$$/g' > "temp_$2"
-sed 's/{/\n$${\n$$/g' $3 | sed 's/}/\n$$}\n$$/g' > "temp_$3"
+sed 's/{/\n$$$$$$${\n$$$$$$$/g' $1 | sed 's/}/\n$$$$$$$}\n$$$$$$$/g' | sed 's/(/\n$$$$$$$(\n$$$$$$$/g' | sed 's/)/\n$$$$$$$)\n$$$$$$$/g' | sed 's/,/\n$$$$$$$,\n$$$$$$$/g' > "temp_$1"
+sed 's/{/\n$$$$$$${\n$$$$$$$/g' $2 | sed 's/}/\n$$$$$$$}\n$$$$$$$/g' | sed 's/(/\n$$$$$$$(\n$$$$$$$/g' | sed 's/)/\n$$$$$$$)\n$$$$$$$/g' | sed 's/,/\n$$$$$$$,\n$$$$$$$/g' > "temp_$2"
+sed 's/{/\n$$$$$$${\n$$$$$$$/g' $3 | sed 's/}/\n$$$$$$$}\n$$$$$$$/g' | sed 's/(/\n$$$$$$$(\n$$$$$$$/g' | sed 's/)/\n$$$$$$$)\n$$$$$$$/g' | sed 's/,/\n$$$$$$$,\n$$$$$$$/g' > "temp_$3"
 
 diff3 -m "temp_$1" "temp_$2" "temp_$3" > mid_merged
 
@@ -10,19 +10,12 @@ rm "temp_$1"
 rm "temp_$2"
 rm "temp_$3"
 
-process_merge_code="
-import sys\n\nfile_to_open = sys.argv[1]\nmerged_file_to_write = sys.argv[2]\n\nmerged_file = open(file_to_open, 'r')\n\nlines = merged_file.readlines()\nfile_s = ''.join(lines)\n\nfile_ret = file_s.replace('\\\\n\$\$', '')\n\nfile2 = open(merged_file_to_write, 'w')\nfile2.write(file_ret)\nfile2.close()"
-
-echo $process_merge_code > process_merge_code.py
-
-python3 process_merge_code.py mid_merged merged
-
-rm process_merge_code.py
+sed ':a;N;$!ba;s/\n\$\$\$\$\$\$\$//g' mid_merged > merged
 
 rm mid_merged
 
-sed "s/temp_$1/$1/g" merged | sed "s/temp_$2/$2/g" | sed "s/temp_$3/$3/g"
+sed "s/temp_$1/$1/g" merged | sed "s/temp_$2/$2/g" | sed "s/temp_$3/$3/g" | sed "s/\(<<<<<<< $1\)\(.\+\)/\1\n\2/" | sed "s/\(||||||| $2\)\(.\+\)/\1\n\2/" | sed "s/\(=======\)\(.\+\)/\1\n\2/" | sed "s/\(>>>>>>> $3\)\(.\+\)/\1\n\2/"
 
-rm merged
+ rm merged
 
 
