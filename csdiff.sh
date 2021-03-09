80a7c42ea531e6364483726825a3a1ad8e041567
Jônatas Clementino <jonatas.clementino@luizalabs.com>
2021-03-08 21:47:43 -0300

    csdiff: fix csdiff when working with multi directory file script

--- csdiff.sh
+++ csdiff.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-sed 's/{/\n$$$$$$${\n$$$$$$$/g' $1 | sed 's/}/\n$$$$$$$}\n$$$$$$$/g' | sed 's/(/\n$$$$$$$(\n$$$$$$$/g' | sed 's/)/\n$$$$$$$)\n$$$$$$$/g' | sed 's/,/\n$$$$$$$,\n$$$$$$$/g' > "temp_$1"
-sed 's/{/\n$$$$$$${\n$$$$$$$/g' $2 | sed 's/}/\n$$$$$$$}\n$$$$$$$/g' | sed 's/(/\n$$$$$$$(\n$$$$$$$/g' | sed 's/)/\n$$$$$$$)\n$$$$$$$/g' | sed 's/,/\n$$$$$$$,\n$$$$$$$/g' > "temp_$2"
-sed 's/{/\n$$$$$$${\n$$$$$$$/g' $3 | sed 's/}/\n$$$$$$$}\n$$$$$$$/g' | sed 's/(/\n$$$$$$$(\n$$$$$$$/g' | sed 's/)/\n$$$$$$$)\n$$$$$$$/g' | sed 's/,/\n$$$$$$$,\n$$$$$$$/g' > "temp_$3"
+sed 's/{/\n$$$$$$${\n$$$$$$$/g' $1 | sed 's/}/\n$$$$$$$}\n$$$$$$$/g' | sed 's/(/\n$$$$$$$(\n$$$$$$$/g' | sed 's/)/\n$$$$$$$)\n$$$$$$$/g' | sed 's/,/\n$$$$$$$,\n$$$$$$$/g' > "$1_temp"
+sed 's/{/\n$$$$$$${\n$$$$$$$/g' $2 | sed 's/}/\n$$$$$$$}\n$$$$$$$/g' | sed 's/(/\n$$$$$$$(\n$$$$$$$/g' | sed 's/)/\n$$$$$$$)\n$$$$$$$/g' | sed 's/,/\n$$$$$$$,\n$$$$$$$/g' > "$2_temp"
+sed 's/{/\n$$$$$$${\n$$$$$$$/g' $3 | sed 's/}/\n$$$$$$$}\n$$$$$$$/g' | sed 's/(/\n$$$$$$$(\n$$$$$$$/g' | sed 's/)/\n$$$$$$$)\n$$$$$$$/g' | sed 's/,/\n$$$$$$$,\n$$$$$$$/g' > "$3_temp"
 
-diff3 -m "temp_$1" "temp_$2" "temp_$3" > mid_merged
+diff3 -m "$1_temp" "$2_temp" "$3_temp" > mid_merged
 
-rm "temp_$1"
-rm "temp_$2"
-rm "temp_$3"
+rm "$1_temp"
+rm "$2_temp"
+rm "$3_temp"
 
 sed ':a;N;$!ba;s/\n\$\$\$\$\$\$\$//g' mid_merged > merged
 
 rm mid_merged
 
-sed "s/temp_$1/$1/g" merged | sed "s/temp_$2/$2/g" | sed "s/temp_$3/$3/g" | sed "s/\(<<<<<<< $1\)\(.\+\)/\1\n\2/" | sed "s/\(||||||| $2\)\(.\+\)/\1\n\2/" | sed "s/\(=======\)\(.\+\)/\1\n\2/" | sed "s/\(>>>>>>> $3\)\(.\+\)/\1\n\2/"
+ESCAPED_LEFT=$(printf '%s\n' "$1" | sed -e 's/[\/&]/\\&/g')
+ESCAPED_BASE=$(printf '%s\n' "$2" | sed -e 's/[\/&]/\\&/g')
+ESCAPED_RIGHT=$(printf '%s\n' "$3" | sed -e 's/[\/&]/\\&/g')
 
- rm merged
+ESCAPED_TEMP_LEFT=$(printf '%s\n' "$1_temp" | sed -e 's/[\/&]/\\&/g')
+ESCAPED_TEMP_BASE=$(printf '%s\n' "$2_temp" | sed -e 's/[\/&]/\\&/g')
+ESCAPED_TEMP_RIGHT=$(printf '%s\n' "$3_temp" | sed -e 's/[\/&]/\\&/g')
+
+sed "s/\(<<<<<<< $ESCAPED_TEMP_LEFT\)\(.\+\)/\1\n\2/" merged | sed "s/\(||||||| $ESCAPED_TEMP_BASE\)\(.\+\)/\1\n\2/" | sed "s/\(=======\)\(.\+\)/\1\n\2/" | sed "s/\(>>>>>>> $ESCAPED_TEMP_RIGHT\)\(.\+\)/\1\n\2/" | sed "s/$ESCAPED_TEMP_LEFT/$ESCAPED_LEFT/g" | sed "s/$ESCAPED_TEMP_BASE/$ESCAPED_BASE/g" | sed "s/$ESCAPED_TEMP_RIGHT/$ESCAPED_RIGHT/g"
+
+rm merged
 
 
