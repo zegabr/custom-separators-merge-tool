9d5c3e8c1ac05f1e4e095fa37798191017c739f6
Heitor Sammuel Carvalho Souza <heitor.scz@gmail.com>
2021-10-17 16:28:11 -0300

    Add false positive java code samples

---
+++ samples/false_positive/left.java
@@ -0,0 +1,14 @@
+public void testScalarEmptyToTryConvert() throws Exception
+{
+  // body1
+
+  {
+    // body2
+  }
+}
+
+/*
+/************************************************************************
+/* Test methods, failed coercions from empty String
+/************************************************************************
+*/
