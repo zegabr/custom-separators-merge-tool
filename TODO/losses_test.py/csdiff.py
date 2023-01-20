6e1c15371a9fea9170b2ac1c45fddca1e6cdd88f
zegabr <jgsp2@cin.ufpe.br>
2023-01-20 20:10:34 -0300

    modify v3 to ignore commented lines

--- TODO/losses_test.py/csdiff.py
+++ TODO/losses_test.py/csdiff.py
@@ -11,10 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# 
-=======
-
-=======================================================================
+# ==============================================================================
 """Tests for Keras loss functions."""
 
 from absl.testing import parameterized
