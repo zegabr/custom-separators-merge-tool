63e0315dd2bf86bd3bc18dfb9213a7a902768afc
zegabr <jgsp2@cin.ufpe.br>
2023-01-20 19:45:20 -0300

    setup files to test debug_v3

---
+++ debug_v3.sh
@@ -0,0 +1,3 @@
+
+bash csdiff_v3.sh -s ": ( ) ," TODO/losses.py/left.py TODO/losses.py/base.py  TODO/losses.py/right.py
+bash csdiff_v3.sh -s ": ( ) ," TODO/losses_test.py/left.py TODO/losses_test.py/base.py  TODO/losses_test.py/right.py
